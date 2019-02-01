c6507f456ac7c48af5f124a9ced9ed0393db2604
irvingsl <irving.lima@vitavr.com>
2019-02-01 15:14:16 -0300

    Finishing the app, creating hotkeys and custom css

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
+export default props => {
 
-)
+        const renderRows = () => {
+            const list = props.list || []
+            return list.map(todo => (
+                <tr key={todo._id}>
+                    <td className={todo.done ? 'Done' : ''}>{todo.description}</td>
+                    <td>
+                        <IconButton style='success' icon='check' hide={todo.done}
+                            onClick={() => props.handleDone(todo)}></IconButton>
+                        <IconButton style='warning' icon='undo' hide={!todo.done}
+                            onClick={() => props.handlePending(todo)}></IconButton>                    
+                        <IconButton style='danger' icon='trash-o'
+                            onClick={() => props.handleRemove(todo)}></IconButton>
+                    </td>
+                </tr>
+            ))
+        }
+    
+    return (
+    <table className='table'>
+        <thead>
+            <tr>
+                <th>Descrição</th>
+                <th className='tableActions'>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            {renderRows()}
+        </tbody>
+    </table>
+
+)}
